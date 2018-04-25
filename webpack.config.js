82f7f34532d4961c01cf834dd5130ee977fac674
Michal <michalplebanski@gmail.com>
2018-04-25 17:29:38 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+//webpack.config.js
+
+module.exports = {
+	entry: './src/app.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'app.bundle.js'
+	}
+};
